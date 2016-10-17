76d2b46c22f9ea743818be0a8cf0fba22b693142
Hamlix <nathanaelle@famille-viller.com>
2016-10-17 17:50:37 +0200

    Ajout du contenu du main

--- src/fr/paris10/td4/FileSystem/FileSystemJ.java
+++ src/fr/paris10/td4/FileSystem/FileSystemJ.java
@@ -1,6 +1,14 @@
 package fr.paris10.td4.FileSystem;
+
+import fr.paris10.td4.AbstractFile.Factory.LinuxFactory;
+import fr.paris10.td4.AbstractFile.Factory.WinFactory;
+
 /**
  * Created by galax on 17/10/2016.
  */
 public class FileSystemJ {
+    LinuxFactory myLinuxDirectory = new LinuxFactory(0);
+    LinuxFactory myLinuxFile = new LinuxFactory(1);
+    WinFactory myWinDirectory = new WinFactory(0);
+    WinFactory myWinFile = new WinFactory(1);
 }
