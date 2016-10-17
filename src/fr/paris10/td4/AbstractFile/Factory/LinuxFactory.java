70578f163e0fd011649283997742c768b82680bf
Hamlix <nathanaelle@famille-viller.com>
2016-10-17 17:50:21 +0200

    Ajout des constructeurs  des Factorys

--- src/fr/paris10/td4/AbstractFile/Factory/LinuxFactory.java
+++ src/fr/paris10/td4/AbstractFile/Factory/LinuxFactory.java
@@ -2,11 +2,26 @@ package fr.paris10.td4.AbstractFile.Factory;
 
 import fr.paris10.td4.AbstractFile.AbstractFile;
 import fr.paris10.td4.AbstractFile.AbstractFileFactory;
+import fr.paris10.td4.AbstractFile.Directory.LinuxDirectory;
+import fr.paris10.td4.AbstractFile.File.LinuxOrdinaryFile;
 
 /**
  * Created by galax on 17/10/2016.
  */
 public class LinuxFactory implements AbstractFileFactory {
+    private LinuxDirectory myDirectory;
+    private LinuxOrdinaryFile myFile;
+
+    public LinuxFactory(int object) {
+        if(object == 0){
+            LinuxDirectory tmp = new LinuxDirectory();
+            myDirectory=tmp;
+        }else{
+            LinuxOrdinaryFile tmp = new LinuxOrdinaryFile();
+            myFile=tmp;
+        }
+    }
+
     public AbstractFile createAbstractFile() {
         return null; //// TODO: 17/10/2016
     }
--- src/fr/paris10/td4/AbstractFile/Factory/WinFactory.java
+++ src/fr/paris10/td4/AbstractFile/Factory/WinFactory.java
@@ -3,12 +3,29 @@ package fr.paris10.td4.AbstractFile.Factory;
 
 import fr.paris10.td4.AbstractFile.AbstractFile;
 import fr.paris10.td4.AbstractFile.AbstractFileFactory;
+import fr.paris10.td4.AbstractFile.Directory.WinDirectory;
+import fr.paris10.td4.AbstractFile.File.WinOrdinaryFile;
 
 /**
  * Created by galax on 17/10/2016.
  */
 public class WinFactory implements AbstractFileFactory {
+    private WinOrdinaryFile myFile;
+    private WinDirectory myDirectory;
+
+    public WinFactory(int object) {
+        if(object==0){
+            WinDirectory tmp = new WinDirectory();
+            myDirectory=tmp;
+        }else{
+           WinOrdinaryFile tmp = new WinOrdinaryFile();
+            myFile = tmp;
+        }
+
+    }
+
+
     public AbstractFile createAbstractFile() {
-        return null; //// TODO: 17/10/2016   
+        return null;
     }
 }
