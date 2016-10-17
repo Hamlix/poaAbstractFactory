fef85735d867ef0e589a2a25077acc52e8c8f5c5
Hamlix <nathanaelle@famille-viller.com>
2016-10-17 17:18:54 +0200

    Ajout du sous package File

---
+++ src/fr/paris10/td4/AbstractFile/File/LinuxOrdinaryFile.java
@@ -0,0 +1,12 @@
+package fr.paris10.td4.AbstractFile.File;
+
+import fr.paris10.td4.AbstractFile.AbstractOrdinaryFile;
+
+/**
+ * Created by galax on 17/10/2016.
+ */
+public class LinuxOrdinaryFile implements AbstractOrdinaryFile {
+    public void open() {
+        //// TODO: 17/10/2016  
+    }
+}
---
+++ src/fr/paris10/td4/AbstractFile/File/WinOrdinaryFile.java
@@ -0,0 +1,12 @@
+package fr.paris10.td4.AbstractFile.File;
+
+import fr.paris10.td4.AbstractFile.AbstractOrdinaryFile;
+
+/**
+ * Created by galax on 17/10/2016.
+ */
+public class WinOrdinaryFile implements AbstractOrdinaryFile {
+    public void open() {
+
+    }
+}
