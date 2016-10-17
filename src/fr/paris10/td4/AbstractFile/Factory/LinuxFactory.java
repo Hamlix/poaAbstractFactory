99e67889a227ae13a3d6af93b7745f9ed9c26a6e
Hamlix <nathanaelle@famille-viller.com>
2016-10-17 17:18:30 +0200

    Ajout du sous package Factory

---
+++ src/fr/paris10/td4/AbstractFile/Factory/LinuxFactory.java
@@ -0,0 +1,13 @@
+package fr.paris10.td4.AbstractFile.Factory;
+
+import fr.paris10.td4.AbstractFile.AbstractFile;
+import fr.paris10.td4.AbstractFile.AbstractFileFactory;
+
+/**
+ * Created by galax on 17/10/2016.
+ */
+public class LinuxFactory implements AbstractFileFactory {
+    public AbstractFile createAbstractFile() {
+        return null; //// TODO: 17/10/2016
+    }
+}
