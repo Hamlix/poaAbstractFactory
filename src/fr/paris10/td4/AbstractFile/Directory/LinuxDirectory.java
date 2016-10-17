66ca39ededad441bc7b8e45e08be7f46560747d2
Hamlix <nathanaelle@famille-viller.com>
2016-10-17 17:18:05 +0200

    Ajout du sous package Directory

---
+++ src/fr/paris10/td4/AbstractFile/Directory/LinuxDirectory.java
@@ -0,0 +1,12 @@
+package fr.paris10.td4.AbstractFile.Directory;
+
+import fr.paris10.td4.AbstractFile.AbstractDirectory;
+
+/**
+ * Created by galax on 17/10/2016.
+ */
+public class LinuxDirectory implements AbstractDirectory {
+    public void open() {
+
+    }
+}
---
+++ src/fr/paris10/td4/AbstractFile/Directory/WinDirectory.java
@@ -0,0 +1,12 @@
+package fr.paris10.td4.AbstractFile.Directory;
+
+import fr.paris10.td4.AbstractFile.AbstractDirectory;
+
+/**
+ * Created by galax on 17/10/2016.
+ */
+public class WinDirectory implements AbstractDirectory {
+    public void open() {
+
+    }
+}
